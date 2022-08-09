56c9fb964cac6a1907e26249919e1e481183e78b
Colleen Que <quecolleen@gmail.com>
2022-08-09 11:01:55 -0600

    fixed dark mode

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,22 +6,31 @@
     //corresponds to id
 
     var darkButton = $("#darkMode"),
-    body =$("#main-body")
+        body = $("#main-body"),
+         element = document.body;
 
     function myFunction() {
-        var element = document.body;
+ 
 
         //set dark = true if contains class dark-mode
+        element.classList.toggle("dark-mode");
+
         var x = document.getElementById("main-body").classList.contains("dark-mode")
         localStorage.setItem('dark', x);
 
-        element.classList.toggle("dark-mode");
+
     }
 
 
-    if(localStorage.getItem('dark')) {
-        document.body.classList.add('dark-mode');
-   }
+    if (localStorage.getItem('dark')==="true") {
+        
+        element.classList.add("dark-mode");
+
+    }
+    else {
+        console.log(localStorage.getItem('dark'));
+        document.body.classList.remove('dark-mode');
+    }
 
     darkButton.click(function () {
         myFunction()
