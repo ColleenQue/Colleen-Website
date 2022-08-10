ab1bf2cd87424b08f3114402ff3f21042e8a9292
Colleen Que <quecolleen@gmail.com>
2022-08-09 21:02:00 -0600

    added images

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ configRoutes(app);
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log('Your routes will be running on http://localhost:3000');
 });
 
 // app.listen(3000, () => {
