4e25c6b68dcc0eb3d36993e8c80a69e3100aa46b
Colleen Que <quecolleen@gmail.com>
2022-08-10 14:47:26 -0600

    update

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -1,8 +1,69 @@
+const canvas = document.getElementById('canvas1'),
+    ctx = canvas.getContext('2d')
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const particlesArray = [];
 
-(function ($) {
-    //Let's start writing AJAX calls!
-    //page load
 
-    //corresponds to id
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+})
 
-})(window.jQuery);
+
+
+const mouse = { x: null, y: null }
+
+canvas.addEventListener('click', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+    console.log(mouse.x, mouse.y)
+    drawCircle(event.x, event.y);
+})
+
+canvas.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+class Particle {
+    constructor() {
+        this.x = mouse.x;
+        this.y = mouse.y;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+    }
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+}
+
+
+function init(){
+    for (let i=0;i<100;i++){
+        particlesArray.push(new Particle());
+    }
+}
+init();
+
+
+function drawCircle(x, y) {
+
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, Math.PI * 2);
+    ctx.fill();
+
+
+}
+
--- routes/art.js
+++ routes/art.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('', async (req, res) => {
 
-    var x = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14];
+    var x = [1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13,14,17];
     res.render('pages/medium/art', { artPage: true,art:x });
 
 });
