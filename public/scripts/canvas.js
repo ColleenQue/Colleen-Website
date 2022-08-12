d009354a20bddecc3d518b329eff2342ed84550b
Colleen Que <quecolleen@gmail.com>
2022-08-12 11:03:30 -0600

    added animation

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -10,60 +10,48 @@ window.addEventListener('resize', function () {
     canvas.height = window.innerHeight;
 })
 
-
-
-const mouse = { x: null, y: null }
-
-canvas.addEventListener('click', function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-    console.log(mouse.x, mouse.y)
-    drawCircle(event.x, event.y);
-})
-
-canvas.addEventListener('mousemove', function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-})
-
 class Particle {
     constructor() {
-        this.x = mouse.x;
-        this.y = mouse.y;
-        this.size = Math.random() * 5 + 1;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+        this.x = Math.random()*canvas.width;
+        this.y = Math.random()*canvas.height;
+        this.size = Math.random() * 5 + 1; //between 1 and 6
+        this.speedX = Math.random() * 3 - 1.5; //between 1.5 and -1.5
+        this.speedY = Math.random() * 3 - 1.5; //between 1.5 and -1.5
     }
-    update() {
+    update() { 
         this.x += this.speedX;
         this.y += this.speedY;
     }
 
     draw() {
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = 'rgb(237, 75, 105)';
         ctx.beginPath();
         ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
         ctx.fill();
     }
-
 }
+
 
 
 function init(){
     for (let i=0;i<100;i++){
-        particlesArray.push(new Particle());
+        particlesArray.push(new Particle()); //push to end of array
     }
 }
-init();
+init(); //particles array contains 100 particles 
 
 
-function drawCircle(x, y) {
-
-    ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(x, y, 50, 0, Math.PI * 2);
-    ctx.fill();
-
-
+function handleParticles(){
+    for(let i=0;i<particlesArray.length;i++){
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
 }
 
+function animate(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    handleParticles();
+    requestAnimationFrame(animate);
+}
+
+animate();
