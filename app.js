90b6b31cc1d45d202ed28b38da591f3dc53ce20d
Colleen Que <quecolleen@gmail.com>
2022-12-12 19:53:24 -0500

    reverse

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
+/*
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   console.log('Your routes will be running on http://localhost:3000');
 });
+*/
 
-// app.listen(3000, () => {
-//   console.log("We've now got a server!");
-//   console.log('Your routes will be running on http://localhost:3000');
-// });
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
+});
